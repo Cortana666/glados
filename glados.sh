1fabbab35fde614725f8e4414c40fd2610f6520b
catindog <jian635446559@gmail.com>
2021-06-30 18:51:19 +0800

    修改脚本

--- glados.sh
+++ glados.sh
@@ -3,14 +3,23 @@
 # 2021-06-18
 # glados sign up
 
-hour=9
+time=09
 while true
 do
+    echo $(date "+%Y-%m-%d %H:%M:%S")
     now=$(date "+%H")
-    if [ $now == $hour ]
+    if [ $now == $time ]
     then
-        curl -b 'COOKEI' -d 'token=glados_network' https://glados.rocks/api/user/checkin
-        hour=`expr $hour - 1`
+        curl -b 'COOKIE' -d 'token=glados_network' https://glados.rocks/api/user/checkin
+        time=`expr $time - 1`
+        if [ $time == -1 ]
+        then
+        time=23
+        fi
+        if [ $time -lt 10 ]
+        then
+        time=0$time
+        fi
     fi
-    sleep 3600
+    sleep 60
 done
