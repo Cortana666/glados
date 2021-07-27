ae1960a75c7926975124e6686c67081b438e97ab
catindog <jian635446559@gmail.com>
2021-07-27 14:33:37 +0800

    增加注释

--- glados.sh
+++ glados.sh
@@ -4,35 +4,40 @@
 # glados sign up
 
 getSignTime(){
+    # 获取随机合法小时
     sign_hour=`expr $RANDOM % 24`
     if [ $sign_hour -lt 10 ]
     then
         sign_hour="0$sign_hour"
     fi
 
+    # 获取随机合法分钟
     sign_minute=`expr $RANDOM % 60`
     if [ $sign_minute -lt 10 ]
     then
         sign_minute="0$sign_minute"
     fi
 
+    # 获取随机合法秒
     sign_second=`expr $RANDOM % 60`
     if [ $sign_second -lt 10 ]
     then
         sign_second="0$sign_second"
     fi
 
+    # 组成签到时间并输出
     sign_time="$sign_hour:$sign_minute:$sign_second"
     echo "下次签到时间为$(date "+%Y-%m-%d ")$sign_time"
 }
 
 sign(){
+    # 请求签到接口并记录请求日志
     response=$(curl -b "$1" -d "token=glados_network" https://glados.rocks/api/user/checkin)
-    $response >> api.log 2>&1
+    echo $response >> api.log 2>&1
 
+    # 匹配签到结果
     message=$(echo $response | grep -Eo "message\":\"([a-zA-Z0-9 !]*)")
     message=${message: 10}
-
     case "$message" in
         "Please Try Tomorrow") message="签到失败，请明天再试"
         ;;
@@ -41,7 +46,11 @@ sign(){
         "Checkin! Get 0 day") message="签到成功，有效期没有延长"
         ;;
     esac
-    echo "$(date "+%Y-%m-%d %H:%M:%S")$message"
+
+    # 输出签到结果并记录签到日志
+    message=$(date "+%Y-%m-%d %H:%M:%S")$message
+    echo $message >> sign.log 2>&1
+    echo $message
 }
 
 main(){
@@ -50,12 +59,14 @@ main(){
 
     while true
     do
+        # 判断是否需要设置明天签到时间
         if [ $is_reset_time == 1 ]
         then
             getSignTime
             is_reset_time=0
         fi
 
+        # 判断跳日
         if [ $(date "+%H") == "00" ]
         then
             is_sign=1
@@ -63,6 +74,7 @@ main(){
 
         now_time=$(date "+%H:%M:%S")
 
+        # 到达时间且今日脚本未签到则签到
         if [ $sign_time == $now_time -a $is_sign == 1 ]
         then
             is_reset_time=1
@@ -70,6 +82,7 @@ main(){
             sign "$1"
         fi
 
+        # 脚本循环睡眠时间
         sleep 0.3
     done
 }
